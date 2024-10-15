2994d5039d64f260aa989af2b098a5f38527d876
IAmMohanad <mohanad_94@outlook.com>
2024-10-15 10:00:09 +0100

    Use command line args

--- DiamondKata/Program.cs
+++ DiamondKata/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         var diamondGenerator = new DiamondGenerator(Console.Out);
-        // diamondGenerator.Generate(args[0][0]);
-        diamondGenerator.Generate('z');
+        diamondGenerator.Generate(args[0][0]);
+        // diamondGenerator.Generate('e');
     }
 }
